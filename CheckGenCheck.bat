91c584fe66bb0f4166c05b5d1f811d04dea5ea2e
Nghiad <nghiad@live.com>
2026-03-06 16:41:03 -0800

    Update CheckGenCheck.bat

--- CheckGenCheck.bat
+++ CheckGenCheck.bat
@@ -33,158 +33,184 @@ Echo.
 if "%~1"=="" (
     call :help
 	goto :eof
-	)
+	) else (
+		SET "source=%~1"
+		shift & goto parse
+		)
+		
 
 REM Read Input
 
 :parse
 
-if not "%~1"=="" (
-	SET "Current=%~1"
-	if "%Current%"=="/?" (
-		call :help
-		goto :eof
-		)
-	if not "%Current:~0,1%"=="-" (
-		SET "source=%~1"
-		shift & goto parse
-		) else (
-			if /I "%Current%"=="-m" (
-				SET "source=%~2"
-				shift & shift & goto parse
-				)
+if "%~1"=="" goto :main
 
-			if /I "%Current%"=="-i" (
-				SET "GenFilter=-i"
-				shift & goto parse
-				)
-				
-			if /I "%Current%"=="-q" (
-				SET "GenFilter=-q"
-				shift & goto parse
-				)
-				
-			if /I "%Current%"=="-e" (
-				SET "errorcheck=-e"
-				shift & goto parse
-				)
-				
-			if /I "%Current%"=="-d" (
-				SET "detailed=1"
-				shift & goto parse
-				)
+if /I "%~1"=="-h" (
+    call :help
+    goto :eof
+    )
 
-			if /I "%Current%"=="-s" (
-				SET "search=-s %~2"
-				shift & shift & goto parse
-				)
-			
-			if /I "%Current%"=="-ms" (
-				SET "manualsearch=%~2"
-				shift & shift & goto parse
-				)
-				
-			if /I "%Current%"=="-l" (
-				SET "listmore=-l %~2"
-				shift & shift & goto parse
-				)
+if "%~1"=="/?" (
+	call :help
+	goto :eof
+	)
+		
+if /I "%~1"=="-m" (
+	SET "source=%~2"
+	shift & shift & goto parse
+	)
 
-			if /I "%Current%"=="-nt" (
-				SET "newerthan=-nt %~2"
-				shift & shift & goto parse
-				)
+if /I "%~1"=="-s" (
+	SET "source=%~2"
+	shift & shift & goto parse
+	)
 
-			if /I "%Current%"=="-ot" (
-				SET "olderthan=-ot %~2"
-				shift & shift & goto parse
-				)
-			
-			if /I "%Current%"=="-debug" (
-				SET "debug=1"
-				shift & goto parse
-				)			
-REM			shift
-REM			goto parse
-			Echo: Invalid Option Selected: %Current%
-			goto :eof
-			)
-	) else (goto :main)
+if /I "%~1"=="-i" (
+	SET "GenFilter=-i"
+	shift & goto parse
+	)
+	
+if /I "%~1"=="-q" (
+	SET "GenFilter=-q"
+	shift & goto parse
+	)
+	
+if /I "%~1"=="-e" (
+	SET "errorcheck=1"
+	shift & goto parse
+	)
+	
+if /I "%~1"=="-d" (
+	SET "detailed=1"
+	shift & goto parse
+	)
+	
+if /I "%~1"=="-l" (
+	SET "listmore=-l %~2"
+	shift & shift & goto parse
+	)
+
+if /I "%~1"=="-nt" (
+	SET "newerthan=-nt %~2"
+	shift & shift & goto parse
+	)
+
+if /I "%~1"=="-ot" (
+	SET "olderthan=-ot %~2"
+	shift & shift & goto parse
+	)
+
+if /I "%~1"=="-debug" (
+	SET "debug=1"
+	shift & goto parse
+	)
+	
+shift
+goto parse
+
 
 :main
 
-if defined debug (
-	call :debug
-	@echo on
-	)
 
 if not defined source (
 	Echo Did not specify source, exiting...
 	goto :eof
 	)
-	
-if defined manualsearch (
-	call :search
-	goto :eof
-	)
-
-Echo.
-Echo ------ !source! Configurations ------
-Echo.
-FOR /F "tokens=* delims=" %%I IN ('findstr /I "!source!" "%ALI_SITE_CONFIG_PATH%\*.site" 2^>nul') DO (
-	Echo %%I
-	)
 
 Echo.
 Echo: -------- Log Times --------
 Echo.
-FOR /F "tokens=3" %%A in ('gencheck !source! !GenFilter! !search! !listmore! !newerthan! !olderthan! ^| grep gen_ ') DO (
+FOR /F "tokens=3" %%A in ('gencheck !source! !GenFilter! !listmore! !newerthan! !olderthan! ^| grep gen_ ') DO (
 	SET "foundlog=%%~nxA"
 	FOR %%D in ('grep "Trace level has been set to tr_LOW" %%A') DO (SET "lowlogging=1")
 	Echo %%A:
 	Echo.
-	if /I "%foundlog:~0,4%"=="igen" (
+	if /I "!foundlog:~0,4!"=="igen" (
 		if defined detailed (
 			FOR /F "tokens=3*" %%X in ('grep "Association handle is valid" %%A') DO (
 				Echo %%X %%Y
 				)
-			FOR /F "tokens=3*" %%X in ('findstr /C:" It took " %%A 2^>nul') DO (
+				
+REM LOW LEVEL LOGS OR BETTER TIMER LOGS WITH MULTIPLE OR SEARCHES
+				
+			FOR /F "tokens=3*" %%X in ('findstr /C:" It took " %%A ^| findstr /V /C:"0.000000" 2^>nul') DO (
 				Echo %%X %%Y
 				)
+				
+				
+
 			FOR /F "tokens=3*" %%X in ('grep "seconds before closing the study folder" %%A') DO (
 				Echo %%X %%Y
 				)
+			Echo.
 			)
 		FOR /F "delims=" %%D in ('grep -A 17 "Importer Time Log :" %%A') DO (
 			SET "foundtime=1"
 			Echo %%D
 			)
 		)
-	if /I "%foundlog:~0,4%"=="qgen" (
+	if /I "!foundlog:~0,4!"=="qgen" (
 		if defined detailed (
 			FOR /F "tokens=3*" %%X in ('grep "Association handle is valid" %%A') DO (
 				Echo %%X %%Y
 				)
-			FOR /F "tokens=3*" %%M in ('grep -A 2 " Query Level = " %%A') DO (
-				Echo %%M %%N
+			FOR /F "tokens=3,4,5,6,7*" %%M in ('grep -A 2 " Query Level = " %%A') DO (
+				Echo %%M %%N %%O %%P %%Q %%R
+				SET "querylevel=%%Q"
 				)
-			FOR /F "delims=" %%M in ('grep -A 6 "Query filter attributes" %%A ^| findstr /R /V "^[{[<]" 2^>nul') DO (
-				Echo %%M %%N
+			Echo.
+			if defined lowlogging (
+REM LOW LEVEL LOGS
+				FOR /F "delims=" %%M in ('grep "Query filter attributes" %%A') DO (
+					SET "qfilter=1"
+					Echo %%M %%N
+					)	
+				if defined qfilter (
+					FOR /F "delims=" %%M in ('grep -A 16 "Query filter attributes" %%A ^| grep -v "Query\|{\|}\|<\|message:"') DO (
+						Echo %%M %%N
+						)
+					Echo.
+					)
 				)
-			FOR /F "tokens=3*" %%J in ('grep "Fetch returned " %%A') DO (
+			FOR /F "tokens=3*" %%J in ('grep "Fetch returned " %%A ^| grep Stud') DO (
 				Echo %%J %%K
 				)
+			FOR /F "tokens=3*" %%J in ('grep "Total time to initialize PACS" %%A') DO (
+				Echo %%J %%K
+				)
+			FOR /F "tokens=3*" %%J in ('grep "Fetch returned " %%A ^| grep Series') DO (
+				Echo %%J %%K
+				)
+			FOR /F "tokens=3*" %%J in ('grep "Database lookup retrieved " %%A') DO (
+				Echo %%J %%K
+				)
+REM May be high lv logs only?		
 			FOR /F "tokens=3*" %%J in ('grep "to get response from study server" %%A') DO (
 				Echo %%J %%K
 				)
-			FOR /F "tokens=3*" %%O in ('grep -A 4 "requests C-MOVE to destination AE TITLE" %%A') DO (
-				Echo %%O %%P
-				)
-			FOR /F "tokens=3*" %%J in ('grep "Preprocessing time for this image" %%A') DO (
+			FOR /F "tokens=3*" %%J in ('grep " ms to complete C-FIND Responses." %%A') DO (
 				Echo %%J %%K
+				)
+			if querylevel="C-MOVE." (
+REM C-MOVEs ONLY
+				Echo.
+				FOR /F "tokens=3*" %%O in ('grep "requests C-MOVE to destination AE TITLE" %%A') DO (
+					SET "string=%%O %%P"
+					)
+				Echo !string!
+				FOR /F "tokens=3*" %%O in ('grep "as C-MOVE destination because AE Title Matching is enabled" %%A') DO (
+					SET "string=%%O %%P"
+					)
+				Echo !string!
+				Echo.
+				FOR /F "tokens=3*" %%J in ('grep "Preprocessing time for this image" %%A') DO (
+					Echo %%J %%K
+					)
 				)
 			FOR /F "tokens=3*" %%J in ('grep "Query Server child process finished handling DICOM association" %%A') DO (
 				Echo %%J %%K
 				)
+			Echo.
 			)
 		)
 		FOR /F "delims=" %%D in ('grep -A 21 "QueryServer Time Log" %%A') DO (
@@ -198,20 +224,47 @@ FOR /F "tokens=3" %%A in ('gencheck !source! !GenFilter! !search! !listmore! !ne
 		Echo.
 		Echo ===== Error Check =====
 		Echo.
-		FOR /F "tokens=1,2,4*" %%D in ('findstr /B "ERROR" %%A ^| grep -v "Could not locate cf variable" 2^>nul') DO (
-			SET "founderror=1"
-			Echo %%D %%E %%F %%G
+		if defined detailed (
+			FOR /F "tokens=1,2,4*" %%D in ('findstr /B "ERROR" %%A 2^>nul') DO (
+				SET "founderror=1"
+				Echo %%D %%E %%F %%G
+				)
+		) else (
+			FOR /F "tokens=1,2,4*" %%D in ('findstr /B "ERROR" %%A ^| grep -v "Could not locate cf variable" ^| grep -v "'Configuration Error': Failed to read" 2^>nul') DO (
+				SET "founderror=1"
+				Echo %%D %%E %%F %%G
+				)
 			)
 		if not defined founderror (
 			Echo No ERRORs found in this log
 			)
 		)
 	Echo.
+	SET "string="
+	SET "qfilter="
+	SET "querylevel="
+	SET "lowlogging="
 	Echo: -----------------------
 	)
+
 if not defined foundlog (
 	Echo GenCheck for !source! did not find any logs
 	goto :eof
+	)
+	
+if defined errorcheck (
+	Echo.
+	Echo ------ !source! Configurations ------
+	Echo.
+	FOR /F "tokens=5* delims=\: " %%I IN ('findstr /I "!source!" "%ALI_SITE_CONFIG_PATH%\*.site" 2^>nul') DO (
+		Echo %%I: %%J
+		)
+	Echo.
+	Echo -------------------------------------
+	if not defined detailed (
+		Echo.
+		Echo Configuration Errors are ignored; use with -d to show all ERRORs
+		)
 	)
 
 if defined debug (call :debug)
@@ -222,35 +275,19 @@ GOTO :eof
 
 REM Functions
 
-:search
-
-if defined manualsearch (
-	Echo.
-	Echo Searching for !manualsearch! in matches from GenCheck !source!:
-	Echo.
-	FOR /F "tokens=3" %%A in ('gencheck !source! !GenFilter! !listmore! !newerthan! !olderthan! ^| grep gen_ ') DO (
-		FOR %%D in ('grep -i !manualsearch! %%A') DO (
-			SET "foundsearch=1"
-			Echo %%A
-			)
-		)
-	if not defined foundsearch (
-		Echo Did not find !manualsearch! in any logs from GenCheck !source!
-		)
-	)
-
-exit /b
-
 :debug
 
 Echo source:       !source!
 Echo GenFilter:    !GenFilter!
-Echo search:       !search!
-Echo manualsearch: !manualsearch!
 Echo listmore:     !listmore!
 Echo newerthan:    !newerthan!
 Echo olderthan:    !olderthan!
 Echo lowlogging:   !lowlogging!
+Echo foundlog:     !foundlog!
+Echo foundtime:    !foundtime!
+Echo querylevel:   !querylevel!
+Echo qfilter:      !qfilter!
+Echo string:       !string!
 
 exit /b
 
@@ -261,21 +298,19 @@ Echo: CheckGenCheck Tool Help Page
 Echo.
 Echo: Description: Quickly pull information directly from a GenCheck
 Echo.
-Echo "Usage: CheckGenCheck [-m] <source> [-i|-q] [-d] [-e] [-l <value>] [-s|-ms <search term>] [-nt|-ot <time metric>]"
+Echo "Usage: CheckGenCheck [-m|-s] <source> [-i|-q] [-d] [-e] [-l <value>] [-nt|-ot <time metric>]"
 Echo.
 Echo: Options:
 Echo: [required] <source>
 Echo: [Optional] -m  Specifies <source>
+Echo: [Optional] -s  Specifies <source>
 Echo: [Optional] -i  Only search iGens
 Echo: [Optional] -q  Only search qGens
 Echo: [Optional] -e  Search ERRORs in igen/qgen
 Echo: [Optional] -d  Show more details per igen/qgen
-Echo: [Optional] -s  Search term through all logs [Testing]
-Echo: [Optional] -ms Manual Search [Testing]
 Echo: [Optional] -l  Specifies how many log matches to search; default is 10 matches
 Echo: [Optional] -nt Filters for logs newer than <time metric>
 Echo: [Optional] -ot Filters for logs older than <time metric>
-Echo: [Optional] -ms Manual Search [Testing]
 Echo: [Optional] -debug
 
 exit /b
